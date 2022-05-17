78da665da631637d9eb1e87fac1c56cdcb50a553
oseunghyun <ohsh1477@gmail.com>
2022-05-17 22:17:40 +0900

    env 환경 변수 파일 설정

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: process.env.VUE_APP_API_URL,
 });
 
 function registerUser(userData) {
-  // const url = "http://localhost:3000/signup";
-  // return axios.post(url, userData);
   return instance.post("signup", userData);
 }
 
