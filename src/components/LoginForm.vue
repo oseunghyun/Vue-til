0b67465ef511b90573fd106e496e0ea41bd265a3
oseunghyun <ohsh1477@gmail.com>
2022-05-17 22:54:53 +0900

    로그인 에러 처리

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -25,14 +25,24 @@ export default {
   },
   methods: {
     async submitForm() {
-      const userData = {
-        username: this.username,
-        password: this.password,
-      };
-      const { data } = await loginUser(userData);
-      console.log(data);
-      this.logMessage = `${data.user.username} 님 환영합니다.`;
-      this.initForm();
+      try {
+        // 비즈니스 로직
+        const userData = {
+          username: this.username,
+          password: this.password,
+        };
+        const { data } = await loginUser(userData);
+        console.log(data);
+        this.logMessage = `${data.user.username} 님 환영합니다.`;
+        // this.initForm();
+      } catch (error) {
+        // 에러 핸들링할 코드
+        // console.log(error.response);
+        this.logMessage = error.response.data;
+        // this.initForm();
+      } finally {
+        this.initForm();
+      }
     },
     initForm() {
       this.username = "";
