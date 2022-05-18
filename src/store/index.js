53c875482e31eddab880094ccb41410b50dee15d
oseunghyun <ohsh1477@gmail.com>
2022-05-18 17:39:43 +0900

    브라우저로 저장소로 인증 값 보존 처리

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import {
+  getAuthFromCookie,
+  getUserFromCookie,
+  saveAuthToCookie,
+  saveUserToCookie,
+} from "@/utils/cookies";
+import { loginUser } from "@/api/index";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    username: "",
-    token: "",
+    username: getUserFromCookie() || "",
+    token: getAuthFromCookie() || "",
   },
   // computed와 유사
   getters: {
@@ -24,6 +31,17 @@ export default new Vuex.Store({
     },
     setToken(state, token) {
       state.token = token;
+    },
+  },
+  actions: {
+    async LOGIN({ commit }, userData) {
+      const { data } = await loginUser(userData);
+      console.log(data.token);
+      commit.commit("setToken", data.token);
+      commit.commit("setUsername", data.user.username);
+      saveAuthToCookie(data.token);
+      saveUserToCookie(data.user.username);
+      return data;
     },
   },
 });
