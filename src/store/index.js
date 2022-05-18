6f84a5850f148093ebdab84377a078dca4bd9139
oseunghyun <ohsh1477@gmail.com>
2022-05-18 17:50:14 +0900

    오타 수정

--- src/store/index.js
+++ src/store/index.js
@@ -37,8 +37,8 @@ export default new Vuex.Store({
     async LOGIN({ commit }, userData) {
       const { data } = await loginUser(userData);
       console.log(data.token);
-      commit.commit("setToken", data.token);
-      commit.commit("setUsername", data.user.username);
+      commit("setToken", data.token);
+      commit("setUsername", data.user.username);
       saveAuthToCookie(data.token);
       saveUserToCookie(data.user.username);
       return data;
